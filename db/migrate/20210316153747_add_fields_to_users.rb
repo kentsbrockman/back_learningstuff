db1a2da096e3f82009b49aa4e37ecf65a9e7ffbf
Quentin PLAUD <qplaud@gmail.com>
2021-03-16 16:41:27 +0100

    Custom fields for User migration ok

---
+++ db/migrate/20210316153747_add_fields_to_users.rb
@@ -0,0 +1,12 @@
+class AddFieldsToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :first_name, :string, null: false
+    add_column :users, :last_name, :string, null: false
+    add_column :users, :is_approved, :boolean, default: false
+    add_column :users, :role, :string, default: "student"
+    add_column :users, :description, :text
+    add_column :users, :linkedin_address, :string
+    add_column :users, :job, :string
+    add_column :users, :stripe_id, :string
+  end
+end
