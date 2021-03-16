fa5bc2d1237342afaf30599365f2c213bbe3344b
Quentin PLAUD <qplaud@gmail.com>
2021-03-16 16:53:10 +0100

    Tweak in Devise controller to let user register with first name and last name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   def render_resource(resource)
     if resource.errors.empty?
       render json: resource
@@ -9,15 +11,20 @@ class ApplicationController < ActionController::API
 
   def validation_error(resource)
     render json: {
-             errors: [
-               {
-                 status: '400',
-                 title: 'Bad Request',
-                 detail: resource.errors,
-                 code: '400'
-               }
-             ]
-           },
-           status: :bad_request
+      errors: [
+        {
+          status: '400',
+          title: 'Bad Request',
+          detail: resource.errors,
+          code: '400'
+        }
+      ]
+    },
+    status: :bad_request
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |user| user.permit(:first_name, :last_name, :email, :password, :password_confirmation)}
+    devise_parameter_sanitizer.permit(:account_update) { |user| user.permit(:email, :password, :password_confirmation, :current_password)}
   end
 end
