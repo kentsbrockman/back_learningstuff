60830d8e3dc53a0f3c83b8bcf2aa9d6abd08c4b0
Dan <dan.bertrand.hs@gmail.com>
2021-03-18 15:09:11 +0100

    Fixed format in Path Serializer

--- app/serializers/learning_path_serializer.rb
+++ app/serializers/learning_path_serializer.rb
@@ -1,14 +1,10 @@
-
-
 class LearningPathSerializer < ActiveModel::Serializer 
   attributes :id, :title, :price_in_cents, :is_single_course
   has_many :categories
   has_many :subscriptions
   has_many :courses
 
-
   def is_single_course
-
     return true if object.courses.length < 2
     return false
   end
