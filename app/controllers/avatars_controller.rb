978b6569b3ca76e8ff1f8c7e49667b1ceb419e8a
Quentin PLAUD <qplaud@gmail.com>
2021-03-19 03:45:44 +0100

    Avatar serializer to better package the returned avatar object

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -2,5 +2,12 @@ class AvatarsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @user.avatar.attach(params[:avatar])
+
+    if @user.avatar.attached?
+      avatar_serializer = AvatarSerializer.new(avatar: @user.avatar, user: @user)
+      render json: avatar_serializer.serialize_new_avatar
+    else
+      render json: {errors: "No profile picture attached"}, status: 400
+    end
   end
 end
