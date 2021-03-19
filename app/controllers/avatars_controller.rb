4d3696b913ca5c32d63fabfe9278aa03ad488362
Quentin PLAUD <qplaud@gmail.com>
2021-03-19 10:39:56 +0100

    Simplify avatar attach action with no serializer

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -2,10 +2,8 @@ class AvatarsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @user.avatar.attach(params[:avatar])
-
     if @user.avatar.attached?
-      avatar_serializer = AvatarSerializer.new(avatar: @user.avatar, user: @user)
-      render json: avatar_serializer.serialize_new_avatar
+      render json: @user.avatar
     else
       render json: {errors: "No profile picture attached"}, status: 400
     end
