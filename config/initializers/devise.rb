f02f6b1b708cb28735ba8cde1cc92a7a3ece613b
Paul K <azn.baoul@gmail.com>
2021-03-26 15:21:36 +0100

    change to make it work on prod

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -28,7 +28,7 @@ Devise.setup do |config|
     'admin-learningstuff@yopmail.com'
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  config.mailer = 'UserMailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
